76204ad7ab2448e37a4544c0cc82bcee14ce5ce6
m1ke <git@m1ke.me>
2015-01-06 15:46:51 +0000

    init file requires

--- init.php
+++ init.php
@@ -1,14 +1,15 @@
 <?php
-
-require '1site.php';
 require 'array.php';
 require 'common.php';
+require 'data.php';
 require 'date.php';
 require 'db.php';
+require 'display.php';
 require 'emailer.php';
-require 'http.php';
 require 'file.php';
+require 'http.php';
 require 'image.php';
+require 'lists.php';
 require 'map.php';
 require 'math.php';
 require 'output.php';
