40b7ec102bf508ac1264c482ea8dfb3997823010
m1ke <git@m1ke.me>
2015-03-09 09:55:04 +0000

    Added generic master file

--- tests/test.php
+++ tests/test.php
@@ -1,13 +1,2 @@
 <?php
-require_once('../../../../Websites/php/simpletest/autorun.php');
-$s['func-rel-to-script']='../';
-require_once($s['func-rel-to-script'].'_functions.php');
-
-class AllTests extends TestSuite {
-    function __construct(){
-        parent::__construct();
-        $this->addFile('test_functions_string.php');
-        $this->addFile('test_display.php');
-    }
-}
-?>
+// Write a test suite to run all tests in here
