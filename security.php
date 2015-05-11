50db771e339c22cd4e7639b03b58ac96958ec17c
supratim <supratims@gmail.com>
2015-05-11 09:57:00 +0100

    encode and decode now require data and pass

--- security.php
+++ security.php
@@ -2,13 +2,18 @@
 
 /*
     simpleEncode and simpleDecode use openssl_encrypt and openssl_descrypt methods internally.
+    $data - data to be encoded, required param
+    $passwd - a key, use the same key while encoding and decoding, required param
+    $method - aes-128-cbc default
+    $options - 0 default
+    $iv - a non-NULL Initialization Vector, must be 16 chars  
     Use these methods to hide url parameters. Not suitable for passwords as it is more of obfuscation rathern than encryption
 */
 
-function simpleEncode($data, $method='aes-128-cbc', $passwd='s0meSeCreTPa55w0Rd', $options=0, $iv='SomeA1AweS0meK5y') {
+function simpleEncode($data, $passwd, $method='aes-128-cbc', $options=0, $iv='SomeA1AweS0meK5y') {
   return openssl_encrypt ($data, $method, $passwd, $options, $iv);
 }
 
-function simpleDecode($data, $method='aes-128-cbc', $passwd='s0meSeCreTPa55w0Rd', $options=0, $iv='SomeA1AweS0meK5y') {
+function simpleDecode($data, $passwd, $method='aes-128-cbc', $options=0, $iv='SomeA1AweS0meK5y') {
   return openssl_decrypt ($data, $method, $passwd, $options, $iv);
 } 
--- tests/test_security.php
+++ tests/test_security.php
@@ -6,8 +6,9 @@ class TestSecurity extends PHPUnit_Framework_TestCase {
 		$test_array=[
                 	900, '1060','A10090','awes0me@gmail.com','nice_param1', '/?!Rand0m12@*[}', 'simple string with space'
 		];
+		$pass = 'testpassword';
 		foreach($test_array as $test){
-                	$this->assertEquals(simpleDecode(simpleEncode($test)), $test);
+                	$this->assertEquals(simpleDecode(simpleEncode($test,$pass),$pass), $test);
 
 		}
         }
