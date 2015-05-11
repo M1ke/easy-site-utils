87c34a2e4e2d7fe96be1f073e58c6b8d9f34816d
supratim <supratims@gmail.com>
2015-05-11 10:47:53 +0100

    revert back to underscore methods from camelcase

--- security.php
+++ security.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-    simpleEncode and simpleDecode use openssl_encrypt and openssl_descrypt methods internally.
+    simple_encode and simple_decode use openssl_encrypt and openssl_descrypt methods internally.
     $data - data to be encoded, required param
     $passwd - a key, use the same key while encoding and decoding, required param
     $method - aes-128-cbc default
@@ -10,10 +10,10 @@
     Use these methods to hide url parameters. Not suitable for passwords as it is more of obfuscation rathern than encryption
 */
 
-function simpleEncode($data, $passwd, $method='aes-128-cbc', $options=0, $iv='SomeA1AweS0meK5y') {
+function simple_encode($data, $passwd, $method='aes-128-cbc', $options=0, $iv='SomeA1AweS0meK5y') {
   return openssl_encrypt ($data, $method, $passwd, $options, $iv);
 }
 
-function simpleDecode($data, $passwd, $method='aes-128-cbc', $options=0, $iv='SomeA1AweS0meK5y') {
+function simple_decode($data, $passwd, $method='aes-128-cbc', $options=0, $iv='SomeA1AweS0meK5y') {
   return openssl_decrypt ($data, $method, $passwd, $options, $iv);
 } 
--- tests/test_security.php
+++ tests/test_security.php
@@ -2,13 +2,13 @@
 require_once __DIR__.'/../init.php';
 
 class TestSecurity extends PHPUnit_Framework_TestCase {
-        function testSimpleEncode(){
+        function test_simple_encode(){
 		$test_array=[
                 	900, '1060','A10090','awes0me@gmail.com','nice_param1', '/?!Rand0m12@*[}', 'simple string with space'
 		];
 		$pass = 'testpassword';
 		foreach($test_array as $test){
-                	$this->assertEquals(simpleDecode(simpleEncode($test,$pass),$pass), $test);
+                	$this->assertEquals(simple_decode(simple_encode($test,$pass),$pass), $test);
 
 		}
         }
