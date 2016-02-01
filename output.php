e51f84aa2b0427b64230d7028d3caa5196179861
m1ke <git@m1ke.me>
2016-02-01 15:58:21 +0000

    Added array optional to echo_shell

--- output.php
+++ output.php
@@ -1,62 +1,68 @@
 <?php
-function csv_output($data,$filename='download',$to_screen=false){
-	$data=strip_tags($data);
+function csv_output($data, $filename = 'download', $to_screen = false){
+	$data = strip_tags($data);
 	if (!empty($filename) and empty($to_screen)){
 		header('Content-Type: text/csv, charset=utf-8');
-		header('Content-Disposition: inline; filename="'.$filename.'.csv"');
+		header('Content-Disposition: inline; filename="' . $filename . '.csv"');
 	}
 	else {
-		$data='<pre>'.$data.'</pre>';
+		$data = '<pre>' . $data . '</pre>';
 		if (!empty($filename)){
-			$data='<p>File name: '.$filename.'</p>'.$data;
+			$data = '<p>File name: ' . $filename . '</p>' . $data;
 		}
 	}
 	echo $data;
 	die;
 }
 
-function echo_array($array,$return=false,$comment=false){
+function echo_array($array, $return = false, $comment = false){
 	if (!defined('BETA') and empty($_SERVER['SHELL']) and (function_exists('developer') and !developer())){
-		return false;
+		return '';
 	}
-	$html=print_r($array,true);
+	$html = print_r($array, true);
 	if (!empty($_SERVER['SHELL']) and !$return){
-		$html.=PHP_EOL;
+		$html .= PHP_EOL;
 	}
 	else {
-		$html='<pre>'.htmlspecialchars($html).'</pre>';
+		$html = '<pre>' . htmlspecialchars($html) . '</pre>';
 		if ($comment){
-			$html='<!--'.$html.'-->';
+			$html = '<!--' . $html . '-->';
 		}
 	}
 	if ($return){
 		return $html;
 	}
 	echo $html;
+
+	return '';
 }
 
 function echo_html($html){
-	echo '<pre>'.htmlspecialchars($html).'</pre>';
+	echo '<pre>' . htmlspecialchars($html) . '</pre>';
 }
 
-function echo_shell($string,$eol=true){
-	if (empty($_SERVER['SHELL'])){
+function echo_shell($string, $eol = true){
+	if (PHP_SAPI!=='cli'){
 		return false;
 	}
 	if ($string instanceof Exception){
-		$string='E: '.$string->getMessage();
+		$string = 'E: ' . $string->getMessage();
 	}
-	echo $string.($eol ? PHP_EOL : '');
+	if (is_array($string)){
+		$string = print_r($string, true);
+	}
+	echo $string . ($eol ? PHP_EOL : '');
+
 	return true;
 }
 
 function json_out($data){
 	header('Content-type: application/json');
-	$data=json_encode($data);
+	$data = json_encode($data);
 	die($data);
 }
 
-function redirect_perm($url=null){
+function redirect_perm($url = null){
 	header("HTTP/1.1 301 Moved Permanently");
 	redirect($url);
 }
