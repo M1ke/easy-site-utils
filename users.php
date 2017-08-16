9c9434630944e7b00a1a274b1f5c39c46f8d5d87
m1ke <git@m1ke.me>
2017-08-16 15:15:41 +0100

    Remove encrypt method which doesn't do encryption, deprecate rand_pass, add random_token

--- users.php
+++ users.php
@@ -1,54 +1,75 @@
 <?php
-function encrypt($string,&$salt=null){
-	return sha1($salt.$string);
-}
-
-function encrypt_token($string,$length=20,&$salt=null){
-	$token=encrypt($string,$salt);
-	$token=substr($token,-1*$length);
-	return $token;
-}
-
-function get_facebook_cookie($fbid,$fbsecret){
-	$args=array();
-	parse_str(trim($_COOKIE['fbs_'.$fbid],'\\"'),$args);
+function get_facebook_cookie($fbid, $fbsecret){
+	$args = [];
+	parse_str(trim($_COOKIE['fbs_'.$fbid], '\\"'), $args);
 	ksort($args);
-	$payload='';
+	$payload = '';
 	foreach ($args as $key => $value){
 		if ($key!='sig'){
-			$payload.=$key.'='.$value;
+			$payload .= $key.'='.$value;
 		}
 	}
 	if (md5($payload.$fbsecret)!=$args['sig']){
 		return null;
 	}
+
 	return $args;
 }
 
-function make_pass(&$p,$names=null){
-	if (empty($names)){
-		$names=array('input'=>'pass','encrypt'=>'pass','salt'=>'salt','clear'=>'clear');
-	}
-	$clear=$p[$names['input']];
-	$salt=rand_pass();
-	$encrypt=encrypt($clear,$salt);
-	$p[$names['encrypt']]=$encrypt;
-	$p[$names['salt']]=$salt;
-	$p[$names['clear']]=$clear;
-}
-
-function make_pass_rand(&$p,$names=null){
-	$p[!empty($names['input']) ? $names['input'] : 'pass']=rand_pass();
-	make_pass($p,$names);
-}
-
-function rand_pass($length=null,$salt='0123456789abcdef'){
-	$num=rand(0,99999);
-	$num2=rand(10000,99999) ^ rand(10000,99999);
-	$hash=md5($num.$salt.$num2);
+/**
+ * @param int $length
+ * @param string $salt
+ * @return string
+ *
+ * @deprecated This does not generate secure random values.
+ * @deprecated Use random_token (from this lib) for user passwords, or uniqid for identifier strings
+ */
+function rand_pass($length = null, $salt = '0123456789abcdef'){
+	$num = rand(0, 99999);
+	$num2 = rand(10000, 99999) ^ rand(10000, 99999);
+	$hash = md5($num.$salt.$num2);
 	if (empty($length)){
-		$length=rand(8,10);
+		$length = rand(8, 10);
 	}
-	$pass=substr($hash,(-1)*$length);
+	$pass = substr($hash, (-1) * $length);
+
 	return $pass;
+}
+
+/**
+ * Generates secure random alphanumeric strings for use as passwords,
+ * verification keys, salts, auth tokens etc.
+ *
+ * Requires random_int which can be provided by PHP 7 or paragonie/random_compat
+ *
+ * @param int $length
+ * @param bool $include_upper_case (default on, set to false for lower case only)
+ *
+ * @return string
+ * @throws Exception
+ */
+function random_token($length, $include_upper_case = true){
+	if ($length<1 || !is_numeric($length)){
+		throw new Exception("The password generator must be called with a numeric length parameter greater than 0");
+	}
+
+	$max_length = 64;
+	if ($length>$max_length){
+		throw new Exception("The password generator should be called with a string length of 128 or less to avoid accidental long loops. $max_length characters really should be enough for a password");
+	}
+
+	$character_set = array_merge(range('a', 'z'), range('0', '9'));
+	if ($include_upper_case){
+		$character_set = array_merge($character_set, range('A', 'Z'));
+	}
+
+	$set_size = count($character_set);
+	$password_arr = [];
+	for ($n = 0; $n<$length; $n++){
+		$password_arr[$n] = $character_set[random_int(0, $set_size - 1)];
+	}
+
+	$password = implode('', $password_arr);
+
+	return $password;
 }
